2d0a821aef6601bb6537848098a57ffe5a26bf4e
Chris Eichelberger <ceichelberger@ccri.com>
2020-07-11 13:00:49 -0400

    updated composed-curve parser
    Signed-off-by: Chris Eichelberger <ceichelberger@ccri.com>

--- src/main/scala/org/eichelberger/sfc/utils/CompositionParser.scala
+++ src/main/scala/org/eichelberger/sfc/utils/CompositionParser.scala
@@ -1,8 +1,11 @@
 package org.eichelberger.sfc.utils
 
 import com.typesafe.scalalogging.Logging
-import org.eichelberger.sfc.SpaceFillingCurve.{Composable, OrdinalVector, OrdinalNumber, SpaceFillingCurve}
+import org.eichelberger.sfc.SpaceFillingCurve.{Composable, OrdinalNumber, OrdinalVector, SpaceFillingCurve}
 import org.eichelberger.sfc._
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
 import scala.util.parsing.combinator.RegexParsers
 
 /*
@@ -11,43 +14,69 @@ import scala.util.parsing.combinator.RegexParsers
  * - R(2,3,4)
  * - H(2, Z(7))
  * - Z(R(4,H(2,2)),8)
+ *
+ * Examples:
+ *   R(t(15), Z(x(10), y(5)))
+ *   R(Z(x(10), y(5)), t(15))
  */
 object CompositionParser extends RegexParsers {
-  val LPAREN = "("
-  val RPAREN = ")"
+  val dtf = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+  val LPAREN = """\(""".r
+  val RPAREN = """\)""".r
   val COMMA = ","
 
-  val R_CURVE_NAME = """(?i)r""".r
-  val Z_CURVE_NAME = """(?i)z""".r
-  val H_CURVE_NAME = """(?i)h""".r
+  val R_CURVE_NAME = """R""".r
+  val Z_CURVE_NAME = """Z""".r
+  val H_CURVE_NAME = """H""".r
+
+  def intLiteral: Parser[Int] = """\d+""".r ^^ { _.toInt }
+
+  def doubleLiteral: Parser[Double] = """[+\-]?[0-9]*\.?[0-9]+""".r ^^ { _.toDouble }
+
+  def dateLiteral: Parser[DateTime] = """\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\dZ""".r ^^ { s => dtf.parseDateTime(s) }
+
+  def longitude: Parser[Dimension[Double]] = """x""".r ~> LPAREN ~> intLiteral ~ opt(COMMA ~ doubleLiteral ~ COMMA ~ doubleLiteral) <~ RPAREN ^^ {
+    case p ~ None => DefaultDimensions.createLongitude(p.toInt)
+    case p ~ Some(_ ~ min ~ _ ~ max) =>
+      Dimension("x", min, isMinIncluded = true, max, isMaxIncluded = true, p.toLong)
+  }
+
+  def latitude: Parser[Dimension[Double]] = """y""".r ~> LPAREN ~> intLiteral ~ opt(COMMA ~ doubleLiteral ~ COMMA ~ doubleLiteral) <~ RPAREN ^^ {
+    case p ~ None => DefaultDimensions.createLatitude(p.toInt)
+    case p ~ Some(_ ~ min ~ _ ~ max) =>
+      Dimension("y", min, isMinIncluded = true, max, isMaxIncluded = true, p.toLong)
+  }
+
+  case class Bounds(min: String, max: String)
+
+  //Dimension("t", MinDate, isMinIncluded = true, MaxDate, isMaxIncluded = true, 60L)
+  def dateTime: Parser[Dimension[DateTime]] = """t""".r ~> LPAREN ~> intLiteral ~ opt(COMMA ~ dateLiteral ~ COMMA ~ dateLiteral) <~ RPAREN ^^ {
+    case p ~ None => DefaultDimensions.createDateTime(p.toInt)
+    case p ~ Some(_ ~ minDate ~ _ ~ maxDate) =>
+      Dimension("t", minDate, isMinIncluded = true, maxDate, isMaxIncluded = true, p.toLong)
+  }
+
+  def dimension: Parser[Dimension[_]] = longitude | latitude | dateTime
 
   def curveName: Parser[String] = R_CURVE_NAME | Z_CURVE_NAME | H_CURVE_NAME ^^ { _.toString }
 
-  def precision: Parser[Int] = """\d+""".r ^^ { _.toInt }
+  def precision: Parser[Dimension[Long]] = intLiteral ^^ { p => DefaultDimensions.createIdentityDimension(p) }
 
-  case class PrecisionOrCurve(precisionOpt: Option[Int], curveOpt: Option[SpaceFillingCurve])
-
-  def childArg: Parser[PrecisionOrCurve] = (precision | curveParser) ^^ {
-    case p: Int               => PrecisionOrCurve(Some(p), None)
-    case c: SpaceFillingCurve => PrecisionOrCurve(None, Some(c))
-  }
+  def childArg: Parser[Composable] = dimension | precision | curveParser
 
   def curveParser: Parser[ComposedCurve] = curveName ~ LPAREN ~ repsep(childArg, COMMA) ~ RPAREN ^^ {
-    case name ~ lp ~ children ~ rp =>
-      val precisions = OrdinalVector(children.flatMap {
-        case PrecisionOrCurve(Some(p), None) => Seq(p.toLong)
-        case PrecisionOrCurve(None, Some(c)) => c.precisions.toSeq
+    case name ~ _ ~ children ~ _ =>
+      val precisions = OrdinalVector(children.map {
+        case c: SpaceFillingCurve => c.precisions.sum
+        case d: Dimension[_] => d.precision
       }:_*)
       val curve = name match {
         case s: String if s.matches(R_CURVE_NAME.toString()) => new RowMajorCurve(precisions)
         case s: String if s.matches(Z_CURVE_NAME.toString()) => new ZCurve(precisions)
         case s: String if s.matches(H_CURVE_NAME.toString()) => new CompactHilbertCurve(precisions)
       }
-      val childParams: Seq[Composable] = children.map {
-        case PrecisionOrCurve(Some(p), None) => DefaultDimensions.createIdentityDimension(p)
-        case PrecisionOrCurve(None, Some(c)) => c
-      }
-      new ComposedCurve(curve, childParams)
+      new ComposedCurve(curve, children)
     }
 
   def buildWholeNumberCurve(s: String): ComposedCurve = parse(curveParser, s).get
--- src/test/scala/org/eichelberger/sfc/utils/CompositionParserTest.scala
+++ src/test/scala/org/eichelberger/sfc/utils/CompositionParserTest.scala
@@ -1,9 +1,11 @@
 package org.eichelberger.sfc.utils
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.eichelberger.sfc.DefaultDimensions.IdentityDimension
 import org.eichelberger.sfc.SpaceFillingCurve.SpaceFillingCurve
 import org.eichelberger.sfc.SpaceFillingCurve.SpaceFillingCurve
 import org.eichelberger.sfc._
+import org.joda.time.DateTime
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
@@ -15,7 +17,10 @@ class CompositionParserTest extends Specification {
   def parsableCurve(curve: SpaceFillingCurve): String = curve match {
     case c: ComposedCurve =>
       c.delegate.name.charAt(0).toString + c.children.map {
-        case d: Dimension[_]      => d.precision
+        case d: IdentityDimension      => d.precision
+        case d: Dimension[DateTime] if d.min.isInstanceOf[DateTime] => s"t(${d.precision})"
+        case d: Dimension[Double] if d.doubleMin < -100 => s"x(${d.precision})"
+        case d: Dimension[Double] if d.doubleMin > -100 => s"y(${d.precision})"
         case s: SubDimension[_]   => s.precision
         case c: SpaceFillingCurve => parsableCurve(c)
       }.mkString("(", ", ", ")")
@@ -27,7 +32,14 @@ class CompositionParserTest extends Specification {
     val toParse: String = parsableCurve(curve)
     val parsed: ComposedCurve = CompositionParser.buildWholeNumberCurve(toParse)
     val fromParse: String = parsableCurve(parsed)
-    println(s"[CURVE PARSER]\n  Input:  $toParse\n  Output:  $fromParse")
+    println(s"[CURVE PARSER FORWARD]\n  Input:    $toParse\n  Output:  '$fromParse''")
+    toParse == fromParse
+  }
+
+  def eval(toParse: String): Boolean = {
+    val parsed: ComposedCurve = CompositionParser.buildWholeNumberCurve(toParse)
+    val fromParse: String = parsableCurve(parsed)
+    println(s"[CURVE PARSER BACKWARD]\n  Input:  '$toParse'\n  Output:  $fromParse")
     toParse == fromParse
   }
 
@@ -58,6 +70,77 @@ class CompositionParserTest extends Specification {
       eval(R23) must beTrue
       eval(H_2_R23) must beTrue
       eval(Z_R23_2) must beTrue
+
+      1 must_== 1
+    }
+  }
+
+  "curves with explicit dimensions" should {
+
+    "parse without explicit bounds correctly" >> {
+      eval("Z(x(10))") must beTrue
+      eval("Z(x(10), y(5))") must beTrue
+      eval("Z(y(5), x(10))") must beTrue
+      eval("Z(t(15), H(y(5), x(10)))") must beTrue
+      eval("Z(H(y(5), x(10)), t(15))") must beTrue
+
+      1 must_== 1
+    }
+
+    "parse with explicit TIME bounds correctly" >> {
+      val cADefault = CompositionParser.buildWholeNumberCurve("Z(t(10))")
+      val cTDefault: Dimension[DateTime] = cADefault.children.head.asInstanceOf[Dimension[DateTime]]
+      cTDefault.min must_== DefaultDimensions.MinDate
+      cTDefault.max must_== DefaultDimensions.MaxDate
+
+      val dtMin = "1990-01-01T00:00:00.000Z"
+      val dtMax = "2050-12-31T23:59:59.999Z"
+      val cACustom = CompositionParser.buildWholeNumberCurve(s"Z(t(10, $dtMin, $dtMax))")
+      val cTCustom: Dimension[DateTime] = cACustom.children.head.asInstanceOf[Dimension[DateTime]]
+      cTCustom.min must_== CompositionParser.dtf.parseDateTime(dtMin)
+      cTCustom.max must_== CompositionParser.dtf.parseDateTime(dtMax)
+
+      1 must_== 1
+    }
+
+    "parse with explicit LONGITUDE bounds correctly" >> {
+      val cADefault = CompositionParser.buildWholeNumberCurve("Z(x(10))")
+      val cXDefault: Dimension[Double] = cADefault.children.head.asInstanceOf[Dimension[Double]]
+      cXDefault.min must_== DefaultDimensions.dimLongitude.min
+      cXDefault.max must_== DefaultDimensions.dimLongitude.max
+
+      val dxMin = "-39.876"
+      val dxMax = "41.234"
+      val cACustom = CompositionParser.buildWholeNumberCurve(s"Z(x(10, $dxMin, $dxMax))")
+      val cXCustom: Dimension[Double] = cACustom.children.head.asInstanceOf[Dimension[Double]]
+      cXCustom.min must_== dxMin.toDouble
+      cXCustom.max must_== dxMax.toDouble
+
+      1 must_== 1
+    }
+
+    "index points correctly" >> {
+      val dtMin = "1990-01-01T00:00:00.000Z"
+      val dtMax = "2050-12-31T23:59:59.999Z"
+      val curve = CompositionParser.buildWholeNumberCurve(s"Z(t(10, $dtMin, $dtMax), H(x(8), y(7)))")
+
+      println(s"curve:  ${curve.name}")
+      println(s"top precisions:  ${curve.precisions.toSeq.map(_.toString).mkString("[", ",", "]")}")
+
+      val index = curve.pointToIndex(Seq[Any](
+        CompositionParser.dtf.parseDateTime("2020-07-10T12:23:31.000Z"),
+        -79.9,
+        38.2
+      ))
+      println(s"index:  $index")
+
+      val cell = curve.indexToCell(index)
+      println(s"cell:  $cell")
+
+      (index >= 0L) must beTrue
+      (index < (1L << 25)) must beTrue
+
+      1 must_== 1
     }
   }
 }
